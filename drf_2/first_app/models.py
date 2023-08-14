202b2e3160ac505f0f9de4b0b191d8f27d2b9a75
ProgrammerShahJalal <you@example.com>
2023-08-14 18:12:52 +0600

    upscale

--- drf_2/first_app/models.py
+++ drf_2/first_app/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
 class Product(models.Model):
     name = models.CharField(max_length=200)
     description = models.TextField()
